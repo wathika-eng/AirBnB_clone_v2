6a7422d8e08684b65df67fd66442fc0a571bab08
wathika-eng <wathika02@gmail.com>
2024-02-15 10:52:22 +0300

    test

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Create a flask minimal app"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """Test if working"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    """Ensure script runs only when called"""
+    app.run(host="0.0.0.0", port=5000)
